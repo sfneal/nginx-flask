22dac73242704e6072b71b6d898e5d7cde62041d
mrstephenneal <stephen.neal14@gmail.com>
2019-07-22 10:18:01 -0400

    MAKE build and push scripts

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker build -t stephenneal/nginx-flask:1.16-alpine ./1.16-alpine/
+docker build -t stephenneal/nginx-flask:1.16-alpine-v2 ./1.16-alpine-v2/
+docker build -t stephenneal/nginx-flask:1.17-alpine ./1.17-alpine/
+docker build -t stephenneal/nginx-flask:1.17-alpine-v2 ./1.17-alpine-v2/
---
+++ push.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sh ./build.sh
+
+docker push stephenneal/nginx-flask:1.16-alpine
+docker push stephenneal/nginx-flask:1.16-alpine-v2
+docker push stephenneal/nginx-flask:1.17-alpine
+docker push stephenneal/nginx-flask:1.17-alpine-v2
