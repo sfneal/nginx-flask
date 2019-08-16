2870e3c7a559903d0c673f88c3b3badba2ec6add
mrstephenneal <stephen.neal14@gmail.com>
2019-08-16 10:04:44 -0400

    CUT v3 image

--- push.sh
+++ push.sh
@@ -6,4 +6,3 @@ docker push stephenneal/nginx-flask:1.16-alpine
 docker push stephenneal/nginx-flask:1.16-alpine-v2
 docker push stephenneal/nginx-flask:1.17-alpine
 docker push stephenneal/nginx-flask:1.17-alpine-v2
-docker push stephenneal/nginx-flask:1.17-alpine-v3
