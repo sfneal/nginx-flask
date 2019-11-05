003ef5337f31cb6ddba79d65fdcf3e9d4a67ca11
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 13:40:43 -0500

    ADD absolute directory resolving to build and push shell scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t stephenneal/nginx-flask:1.16-alpine ./1.16-alpine/
-docker build -t stephenneal/nginx-flask:1.16-alpine-v2 ./1.16-alpine-v2/
-docker build -t stephenneal/nginx-flask:1.17-alpine ./1.17-alpine/
-docker build -t stephenneal/nginx-flask:1.17-alpine-v2 ./1.17-alpine-v2/
-docker build -t stephenneal/nginx-flask:1.17-alpine-v3 ./1.17-alpine-v3/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t stephenneal/nginx-flask:1.16-alpine "${DIR}"/1.16-alpine/
+docker build -t stephenneal/nginx-flask:1.16-alpine-v2 "${DIR}"/1.16-alpine-v2/
+docker build -t stephenneal/nginx-flask:1.17-alpine "${DIR}"/1.17-alpine/
+docker build -t stephenneal/nginx-flask:1.17-alpine-v2 "${DIR}"/1.17-alpine-v2/
+docker build -t stephenneal/nginx-flask:1.17-alpine-v3 "${DIR}"/1.17-alpine-v3/
--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sh ./build.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sh "${DIR}"/build.sh
 
 docker push stephenneal/nginx-flask:1.16-alpine
 docker push stephenneal/nginx-flask:1.16-alpine-v2
