14fa674265832400c1716b2bca1b67242c0c8a29
mrstephenneal <stephen.neal14@gmail.com>
2020-01-16 10:19:03 -0500

    MAKE v11 image that supports http requests on /api requests

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ docker build -t stephenneal/nginx-flask:1.16-alpine-v2 "${DIR}"/1.16-alpine-v2/
 docker build -t stephenneal/nginx-flask:1.17-alpine "${DIR}"/1.17-alpine/
 docker build -t stephenneal/nginx-flask:1.17-alpine-v2 "${DIR}"/1.17-alpine-v2/
 docker build -t stephenneal/nginx-flask:1.17-alpine-v3 "${DIR}"/1.17-alpine-v3/
+docker build -t stephenneal/nginx-flask:1.17-alpine-v4 "${DIR}"/1.17-alpine-v4/
--- push.sh
+++ push.sh
@@ -9,3 +9,4 @@ docker push stephenneal/nginx-flask:1.16-alpine-v2
 docker push stephenneal/nginx-flask:1.17-alpine
 docker push stephenneal/nginx-flask:1.17-alpine-v2
 docker push stephenneal/nginx-flask:1.17-alpine-v3
+docker push stephenneal/nginx-flask:1.17-alpine-v4
