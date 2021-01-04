18104a00c05af8d0f11318fbec330b33f43dcbf6
Stephen Neal <stephen.neal14@gmail.com>
2021-01-04 12:19:46 -0500

    OPTIMIZE Travis CI config by adding env matrix & param passing to build.sh script
    FIX build.sh to accept arguments.

--- build.sh
+++ build.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
+# Base directory containing source code
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/nginx-flask:1.16-alpine "${DIR}"/1.16-alpine/
-docker build -t stephenneal/nginx-flask:1.16-alpine-v2 "${DIR}"/1.16-alpine-v2/
-docker build -t stephenneal/nginx-flask:1.17-alpine "${DIR}"/1.17-alpine/
-docker build -t stephenneal/nginx-flask:1.17-alpine-v2 "${DIR}"/1.17-alpine-v2/
-docker build -t stephenneal/nginx-flask:1.17-alpine-v3 "${DIR}"/1.17-alpine-v3/
-docker build -t stephenneal/nginx-flask:1.17-alpine-v4 "${DIR}"/1.17-alpine-v4/
-docker build -t stephenneal/nginx-flask:1.18-alpine-v1 "${DIR}"/1.18-alpine-v1/
-docker build -t stephenneal/nginx-flask:1.19-alpine-v1 "${DIR}"/1.19-alpine-v1/
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
+
+# Check if the TAG variable is set
+if [ -z "$TAG" ]
+
+  # Only build one image
+  then
+    docker build -t stephenneal/nginx-flask:"${TAG}" "${DIR}"/"${TAG}"/
+
+  # Build all images
+  else
+    docker build -t stephenneal/nginx-flask:1.16-alpine "${DIR}"/1.16-alpine/
+    docker build -t stephenneal/nginx-flask:1.16-alpine-v2 "${DIR}"/1.16-alpine-v2/
+    docker build -t stephenneal/nginx-flask:1.17-alpine "${DIR}"/1.17-alpine/
+    docker build -t stephenneal/nginx-flask:1.17-alpine-v2 "${DIR}"/1.17-alpine-v2/
+    docker build -t stephenneal/nginx-flask:1.17-alpine-v3 "${DIR}"/1.17-alpine-v3/
+    docker build -t stephenneal/nginx-flask:1.17-alpine-v4 "${DIR}"/1.17-alpine-v4/
+    docker build -t stephenneal/nginx-flask:1.18-alpine-v1 "${DIR}"/1.18-alpine-v1/
+    docker build -t stephenneal/nginx-flask:1.19-alpine-v1 "${DIR}"/1.19-alpine-v1/
+fi
