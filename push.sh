9c660d6add858e201e4924735e775f20b51d26cb
Stephen Neal <stephen.neal14@gmail.com>
2021-03-15 12:51:42 -0400

    ADD 'deploy' section to Travis CI config for pushing Docker images to Docker Hub

--- push.sh
+++ push.sh
@@ -2,13 +2,27 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sh "${DIR}"/build.sh
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
 
-docker push stephenneal/nginx-flask:1.16-alpine
-docker push stephenneal/nginx-flask:1.16-alpine-v2
-docker push stephenneal/nginx-flask:1.17-alpine
-docker push stephenneal/nginx-flask:1.17-alpine-v2
-docker push stephenneal/nginx-flask:1.17-alpine-v3
-docker push stephenneal/nginx-flask:1.17-alpine-v4
-docker push stephenneal/nginx-flask:1.18-alpine-v1
-docker push stephenneal/nginx-flask:1.19-alpine-v1
+# Check if the TAG variable is set
+if [ "$TAG" != null ]
+
+  # Only build & push one image
+  then
+    sh "${DIR}"/build.sh "${TAG}"
+    docker push stephenneal/nginx-flask:"${TAG}"
+
+  # Build & push all images
+  else
+    sh "${DIR}"/build.sh
+
+    docker push stephenneal/nginx-flask:1.16-alpine
+    docker push stephenneal/nginx-flask:1.16-alpine-v2
+    docker push stephenneal/nginx-flask:1.17-alpine
+    docker push stephenneal/nginx-flask:1.17-alpine-v2
+    docker push stephenneal/nginx-flask:1.17-alpine-v3
+    docker push stephenneal/nginx-flask:1.17-alpine-v4
+    docker push stephenneal/nginx-flask:1.18-alpine-v1
+    docker push stephenneal/nginx-flask:1.19-alpine-v1
+fi
