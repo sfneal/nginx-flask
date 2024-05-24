37296b0c443e79d9a7a648673affb55bd3f1b3d3
Stephen Neal <stephen.neal14@gmail.com>
2024-05-24 15:51:40 -0400

    Add support for Nginx v1.26 (#33)

--- build.sh
+++ build.sh
@@ -45,4 +45,5 @@ if [ "$TAG" != null ]
     docker build -t stephenneal/nginx-flask:1.23-alpine "${DIR}"/1.23-alpine/
     docker build -t stephenneal/nginx-flask:1.24-alpine "${DIR}"/1.24-alpine/
     docker build -t stephenneal/nginx-flask:1.25-alpine "${DIR}"/1.25-alpine/
+    docker build -t stephenneal/nginx-flask:1.26-alpine "${DIR}"/1.26-alpine/
 fi
